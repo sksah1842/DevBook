78ae5506b50a92d28b90bceee7e1c57e21342a3f
sksah <sksah1842@gmail.com>
2025-10-02 22:58:02 +0530

    fix error

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import { store } from '../store';
 import { LOGOUT } from '../actions/types';
+import { setAlert } from '../actions/alert';
 
 // Create an instance of axios
 const api = axios.create({
@@ -22,8 +23,20 @@ const api = axios.create({
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response.status === 401) {
+    if (err.response && err.response.status === 401) {
       store.dispatch({ type: LOGOUT });
+    }
+    // Global popup for 4xx/5xx with message payloads
+    if (err.response && err.response.data) {
+      const data = err.response.data;
+      const messages = [];
+      if (typeof data?.error === 'string') messages.push(data.error);
+      if (typeof data?.msg === 'string') messages.push(data.msg);
+      if (Array.isArray(data?.errors)) messages.push(...data.errors.map((e) => e?.msg).filter(Boolean));
+      const message = messages.filter(Boolean).join('\n');
+      if (message) {
+        store.dispatch(setAlert(message, 'danger'));
+      }
     }
     return Promise.reject(err);
   }
